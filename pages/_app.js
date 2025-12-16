d310a82b4d5b45928c0d13cef78adf0d34fda1f1
홍요한 <ghddygks45@gmail.com>
2025-12-16 11:51:32 +0900

    First Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,34 @@
+import Container from "@/components/Container";
+import Header from "@/components/Header";
+import { ThemeProvider } from "@/lib/ThemeContext";
 import "@/styles/globals.css";
+import Head from "next/head";
+import { Noto_Sans_KR } from "@next/font/google";
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ["400", "700"],
+  subsets: [],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <style>
+          {`
+          html{
+            font-family: ${notoSansKR.style.fontFamily}, sans-serif;
+          }`}
+        </style>
+        <title>watchit</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider>
+        <Header />
+        <Container page>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+  );
 }
